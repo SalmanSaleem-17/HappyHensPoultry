29a9f862c57dcc15d3f9abc4ef3a834eac857e56
SalmanSaleem17 <Shanisaleem17@gmail.com>
2026-05-23 01:50:26 +0500

    Update the design Issue for Mobile Screen.

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
       <head>
         <script dangerouslySetInnerHTML={{ __html: noFlashScript }} />
       </head>
-      <body className="min-h-full flex flex-col bg-background text-foreground">
+      <body className="min-h-full flex flex-col bg-background text-foreground overflow-x-hidden">
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
